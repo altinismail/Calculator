81a0cea43168dc20e30a3bceeb730071eb3350aa
Ismail Altintas <altinismail@gmail.com>
2019-01-19 16:55:00 -0500

    add new parameters

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,7 +3,7 @@ public class Calculator {
     public static double add(double num1, double num2) {
         return num1 + num2;
     }
-    public static double subtract(double num1, double num2){
+    public static double subtract(double numA, double numB){
         double result = numA - numB;
         return result;
     }
